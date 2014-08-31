1d653d8649d7fcea8390fb926895e041c6400d43
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-31 17:53:45 -0400

    start templating

--- js/maker.js
+++ js/maker.js
@@ -1,6 +1,13 @@
 (function(){
     function Maker() {
-        console.log("This is Maker");
+        var source = document.getElementById("skeleton").innerHTML;
+        var template = Handlebars.compile(source);
+        var data = {
+            imports: "This is maker"
+        };
+
+        var output = document.getElementById("experiment");
+        output.innerHTML = template(data);
     };
 
     this.Maker = Maker;
