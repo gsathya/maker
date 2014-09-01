9f08034c2da0dbb5a51508b5d38d81f62e6d996f
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-31 20:16:49 -0400

    Handle instance vars

--- js/maker.js
+++ js/maker.js
@@ -1,11 +1,35 @@
 (function(){
     function Maker() {
+        // import the required primitives
+        Handlebars.registerHelper('import', function(items, options) {
+            var out = '';
+
+            for(var i=0; i<items.length; i++) {
+                out += "import centinel.primitives.";
+                out += items[i] + " as " + items[i] + "\n";
+            }
+
+            return out;
+        });
+
+        Handlebars.registerHelper('instance_vars', function(item){
+            var out = '';
+            out += "self." + item.name + " = " + item.value + "\n";
+            return out;
+        });
+
         var source = document.getElementById("skeleton").innerHTML;
         var template = Handlebars.compile(source);
         var data = {
-            imports: "import centinel.primitives.http as http",
+            primitives: ["http", "dnslib"],
+            class_name: "HTTPRequestExperiment",
             name: "http_request",
-            primitive: "http.get_request(line)"
+            vars: [
+                {name: "input_file", value: "input_file"},
+                {name: "results", value: "[]"},
+                {name: "path", value: "'/'"}
+            ],
+            functions: "http.get_request(line)"
         };
 
         var output = document.getElementById("experiment");
