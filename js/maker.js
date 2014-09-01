d68e7495f416d9846b6d7034109d13b738ec280c
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-09-01 19:22:51 -0400

    Remove more redundant code

--- js/maker.js
+++ js/maker.js
@@ -12,25 +12,6 @@
         var source = document.getElementById("skeleton").innerHTML;
         var template = Handlebars.compile(source);
 
-        var data = {
-            primitives: [
-                {name: "http", alias: "http"},
-                {name: "dnslib", alias: "dns"}
-            ],
-            class_name: "HTTPRequestExperiment",
-            name: "http_request",
-            vars: [
-                {name: "input_file", value: "input_file"},
-                {name: "results", value: "[]"},
-                {name: "path", value: "'/'"}
-            ],
-            functions: [{
-                module: "http",
-                method: "get_request",
-                args: "line, self.path"
-            }]
-        };
-
         var editor = ace.edit("editor");
         editor.setTheme("ace/theme/monokai");
         editor.getSession().setMode("ace/mode/python");
